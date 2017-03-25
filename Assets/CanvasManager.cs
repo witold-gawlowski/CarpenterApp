efd4a12c58c7241a8618c40a3bc10eb056a009c2
witold-gawlowski <witek.gawlowski@gmail.com>
2017-03-25 15:08:32 +0000

    Initial commit. UI layout setup.

---
+++ Assets/CanvasManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class CanvasManager : MonoBehaviour {
+  public List<Button> przyciski;
+  public List<GameObject> panele;
+  public Text tytulSekcji; 
+  void Start()
+  {
+    int i = 0;
+    foreach(GameObject go in panele)
+    {
+      Text tekstPrzycisku = przyciski[i].GetComponentInChildren<Text>();
+      tekstPrzycisku.text = go.name;
+      i++;
+    }
+  }
+}
---
+++ Assets/MultiplyScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MultiplyScript : MonoBehaviour {
+  public Text result;
+  public Text multiplier1;
+  public Text multiplier2;
+  public void OnPress(){
+    result.text = (int.Parse(multiplier1.text)*int.Parse(multiplier2.text)).ToString();
+  }
+}
