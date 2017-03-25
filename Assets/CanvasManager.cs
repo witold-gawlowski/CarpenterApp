63e77b43c82bb48504b5f7318dfc021f099ead40
witold-gawlowski <witek.gawlowski@gmail.com>
2017-03-25 19:17:46 +0000

    LAyout almost finished.

--- Assets/CanvasManager.cs
+++ Assets/CanvasManager.cs
@@ -5,7 +5,43 @@ using UnityEngine.UI;
 public class CanvasManager : MonoBehaviour {
   public List<Button> przyciski;
   public List<GameObject> panele;
-  public Text tytulSekcji; 
+  public Text tytulSekcji;
+
+  private GameObject obecnyPanel;
+
+  private void UstawObecynPanel(GameObject panel)
+  {
+    if (obecnyPanel)
+    {
+      obecnyPanel.SetActive(false);
+    }
+    obecnyPanel = panel;
+    panel.SetActive(true);
+    tytulSekcji.text = panel.name;
+  }
+
+
+  public void UstawSekcjeNa(string s)
+  {
+    foreach(GameObject panel in panele)
+    {
+      if (panel.name.CompareTo(s) == 0)
+      {
+        UstawObecynPanel(panel);
+        return;
+      }
+    }
+    print("nie można znaleźć przycisku o tej nazwie");
+  }
+
+  void Awake()
+  {
+    foreach(GameObject panel in panele)
+    {
+      panel.SetActive(false);
+    }
+  }
+
   void Start()
   {
     int i = 0;
@@ -15,5 +51,6 @@ public class CanvasManager : MonoBehaviour {
       tekstPrzycisku.text = go.name;
       i++;
     }
+    przyciski[0].onClick.Invoke();
   }
 }
--- Assets/SkryptPrzycisku.cs
+++ Assets/SkryptPrzycisku.cs
@@ -1,7 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class SkryptPrzycisku : MonoBehaviour {
-
+  public CanvasManager menadzerOkien;
+  void Awake()
+  {
+    menadzerOkien = GetComponentInParent<CanvasManager>();
+  }
+  public void ChangeScreen()
+  {
+    menadzerOkien.UstawSekcjeNa(GetComponentInChildren<Text>().text);
+  }
 }
