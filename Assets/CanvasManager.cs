61d2ff09d10a29f29706f0c79f10f7f08ae9e37d
witold-gawlowski <witek.gawlowski@gmail.com>
2017-03-27 19:04:25 +0100

    Added updates from client. Added code skeleton for calculations.

--- Assets/CanvasManager.cs
+++ Assets/CanvasManager.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 public class CanvasManager : MonoBehaviour {
+  //layout
   public List<Button> przyciski;
   public List<GameObject> panele;
   public Text tytulSekcji;
-
   private GameObject obecnyPanel;
+
+  /* Input fields calling recalculate in this MB.
+   * This triggers recalcualtion of all the output fields referenced in this script.
+   * Recalculation takes place here.  
+   */
+
+  //output fields
+  
 
   private void UstawObecynPanel(GameObject panel)
   {
@@ -40,6 +48,11 @@ public class CanvasManager : MonoBehaviour {
     {
       panel.SetActive(false);
     }
+  }
+
+  public void Recalculate()
+  {
+    
   }
 
   void Start()
